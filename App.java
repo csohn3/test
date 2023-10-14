3c6fafd232dd84bbc7dc8b63408b58a4dcd881f1
Christopher Sohn <csohn3@student.alamo.edu>
2023-10-14 10:24:53 -0500

    Initial commit

---
+++ App.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.Soundbank;
+import java.util.Arrays;
+import java.util.Scanner;
+public class App {
+    public static void main (String[] Args) {
+        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+
+
+    }
+}
